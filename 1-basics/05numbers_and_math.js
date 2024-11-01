52c6eefd4a3a637d60471890b2c8d8b84ac4389e
Debjyoti sarkar <134799724+Debzoti@users.noreply.github.com>
2024-11-01 08:18:05 +0000

    included number methods and math

---
+++ 1-basics/05numbers_and_math.js
@@ -0,0 +1,24 @@
+/*
+//numbers and methods
+const balance = 400
+const newDeposit = new Number(100)
+console.log(newDeposit);
+
+console.log(newDeposit.toFixed(2)); //set the precision value fixed to 2
+const flt = -100.528764
+console.log(flt.toPrecision(7)); // sets the integer numbers as whole not after . precision
+
+
+//maths library
+console.log(Math);
+console.log(Math.abs(flt)); //makes integer positive
+console.log(Math.round(2.5)); // makes it with not decimal
+console.log(Math.floor(2.99)); //choose near low value
+console.log(Math.ceil(1.22)); //chooses near top value
+*/
+console.log(Math.random()); //gives any random value between 0.0 to 0.9
+console.log(Math.random()*10+1); // gives 1 to 10
+
+const min = 100
+const max = 200
+console.log((Math.random()*(max-min+1))+min); // gives integer always in given range
