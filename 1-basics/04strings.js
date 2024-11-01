26a1727c3ca6aa37e278da076dedccbc5c9a67fa
Debjyoti sarkar <134799724+Debzoti@users.noreply.github.com>
2024-11-01 07:40:06 +0000

    more on strings methods

--- 1-basics/04strings.js
+++ 1-basics/04strings.js
@@ -3,7 +3,7 @@ console.log( myName[1]); //acessing each character
 const sirName = new String(`sarkar`)
 console.log(`hey, i am  ${ myName +" "+sirName} nice to meet you`); // use bacticks for strings is a good pratice
 //dive into methods
-/*
+
 console.log(myName.__proto__); // output returns a {} --> its not an empty object
 console.log(sirName.slice(0,3));// slice string starts with index 0 upto 1(n-1) 
 console.log(sirName.slice(-5,3));// it can store negetive values like (-ve,+ve) it starts from opposite side and trim upto second param
@@ -11,6 +11,12 @@ console.log(myName.length); //returns length of the string here its 8
 console.log(sirName.toUpperCase()); // make string uppercase but not the original value
 console.log(myName.charAt(2)); // returns char at index 2
 console.log(myName.includes(`d`)); // returns boolean value if a char is present in the string
-*/
+
 const txt  = "    lol   "
-console.log(txt.trim());  // trimes only white space characters therealso trimstart() and trimend()
+console.log(txt.trim());  // trimes only white space characters theres also trimstart() and trimend()
+
+const url = `https://www.google.co.in/`
+console.log(url.replace('//','..')); // replaces first arg with second arg in string
+console.log(url.split('.',3)); // split with respect to sperator and returns an array of parts next arg is a limit to no of parts you want to store in the array
+
+//checkout all methods on javascript.info
