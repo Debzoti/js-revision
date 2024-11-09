0b33e4145d776c2871cbe1b4e76289ff118e825d
Debjyoti sarkar <134799724+Debzoti@users.noreply.github.com>
2024-11-09 13:49:47 +0000

    ...

---
+++ 3-intermediate/04iterators.js
@@ -0,0 +1,26 @@
+// iterate through objects
+// --> for in loop
+// const user = {
+//     name : "debzoti",
+//     email : "debzoti123@gmail.com",
+//     "pet name" : "vicky",
+//     id : 1
+// }
+// for (const key in user) {
+//     console.log(key, user[key]);
+// }           
+
+
+// // itearte through strings
+// const str = "debzoti"
+// for (const char of str) {
+//     console.log(char);
+// }
+
+// // iterate through arrays
+// const arr = [1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+//
